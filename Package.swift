2f2f2cc743c9c1457615877ce4c354beed848fb2
md_sahil_ak <mdsahilak07@gmail.com>
2019-04-27 03:25:14 +0530

    Cleaned up the default API template

--- Package.swift
+++ Package.swift
@@ -11,10 +11,13 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
         // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
-        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
+        
+        // 🍃 Leaf, a templating language inspired from swift.
+        .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc")
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor", "Leaf"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
